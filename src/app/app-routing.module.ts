84a6011835c80d439c34a357969b3df786751f70
Mike Meier <m0.0m58@yahoo.de>
2023-01-20 18:41:06 +0100

    #

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { AboutmeComponent } from './aboutme/aboutme.component';
 import { SkillsetComponent } from './skillset/skillset.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: 'home', component: HeaderComponent },
   { path: 'aboutme', component: AboutmeComponent },
-  { path: 'skillset', component : SkillsetComponent }
+  { path: 'skillset', component : SkillsetComponent },
+  { path: 'contact', component: ContactComponent }
 ];
 
 @NgModule({
