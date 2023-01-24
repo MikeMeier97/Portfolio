186885a7ec774900aa14eefa9826eac244a0edc6
Mike Meier <m0.0m58@yahoo.de>
2023-01-24 11:39:42 +0100

    some bubbles addet (position)

--- src/app/mywork/mywork.component.ts
+++ src/app/mywork/mywork.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-mywork',
   templateUrl: './mywork.component.html',
   styleUrls: ['./mywork.component.scss']
 })
-export class MyworkComponent {
+export class MyworkComponent implements OnInit{
+  ngOnInit(): void {}
+  numberOfBubbles = 10;
+  bubbles = Array(this.numberOfBubbles).fill(0);
 }
