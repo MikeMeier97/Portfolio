57762a2c940bea72413acfa595d3093ef4485319
Mike Meier <m0.0m58@yahoo.de>
2023-02-01 09:33:31 +0100

    skillset responsive

--- src/app/skillset/skillset.component.ts
+++ src/app/skillset/skillset.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skillset',
   templateUrl: './skillset.component.html',
-  styleUrls: ['./skillset.component.scss']
+  styleUrls: ['./skillset.component.scss'],
 })
-export class SkillsetComponent {
-
+export class SkillsetComponent implements OnInit {
+  public innerWidth: any;
+  ngOnInit(): void {
+    this.innerWidth = window.innerWidth;
+  }
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.innerWidth = window.innerWidth;
+  }
 }
