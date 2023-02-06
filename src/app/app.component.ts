05e6ac789adae702d686a9d92ca7c45ad9ec0183
Mike Meier <m0.0m58@yahoo.de>
2023-02-06 12:45:52 +0100

    mobile menu patch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MyPortfolio';
+  constructor(public heroService: HeroService) {}
 }
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,16 +1,30 @@
-import { Injectable } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
+@Component({
+  selector: 'app-example',
+  template: ''
+})
+
 export class HeroService {
-  mobileMenu: boolean = true; //change
+  mobileMenuHidden: boolean = true; //change
   menuToggle() {
-    if (!this.mobileMenu) {
-      this.mobileMenu = true;
+    if (!this.mobileMenuHidden) { 
+      document.getElementById('menuMobile')?.classList.remove('open_animation-slide');
+      setTimeout(() => {
+        document.getElementById('menuMobile')?.classList.add('d-none');
+        this.document.body.classList.remove('overflow_hidden');
+      }, 1000);
+      this.mobileMenuHidden = true;
     } else {
-      this.mobileMenu = false;
+      this.document.body.classList.add('overflow_hidden');
+      this.mobileMenuHidden = false;
+      document.getElementById('menuMobile')?.classList.remove('d-none');
+      document.getElementById('menuMobile')?.classList.add('open_animation-slide');
     }
   }
-  constructor() {}
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 }
